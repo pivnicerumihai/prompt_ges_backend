1cb82b2b034b23df41a52753f4080e918b94e522
Mihai <mihai@swfy.co.uk>
2023-06-03 14:13:17 +0100

    small_changes

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const { Configuration, OpenAIApi } = require('openai');
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+const cors = require('cors');
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY, organization: 'org-xUjRSuXI3AOAmlpDWMXJEuew' });
-
+app.use(cors());
+app.use(express.json()); 
 const openai = new OpenAIApi(configuration);
 const data = require('./data/help_articles.js')
 
@@ -21,18 +23,19 @@ async function generateChatCompletion(role, content, model='gpt-4', temperature=
   return res.data.choices[0].message.content;
 }
 
-app.get('/', async (req, res) => {
-  const question = "On my mobile app, how can I create a new job and then assign it to a customer via the calendar?";
+app.post('/getResponse', async (req, res) => {
+  console.log(req.body)  
+  const question = req.body.question;  
   let category;
 
   try {
-    const issueCategoryContent = `We provide a Software for Gas Engineers. It has a mobile and a web app version. If you could put the following question from a customer in a category, what category would it be? Question: '${question}', Categories available:'WebApp', 'MobileApp','Issues','Costs', 'Others' Give me just the category name, nothing else.`;
+    const issueCategoryContent = `We provide a Software for Gas Engineers. It has a mobile and a web app version. If you could put the following question from a customer in a category, what category would it be? Question: '${question}', Categories available:'WebApp', 'MobileApp','Costs'. Give me just the category name, nothing else.`;
     category = await generateChatCompletion("user", issueCategoryContent);
 
     console.log("Category is ", category)
 
     if(category === 'Costs' || category === 'WebApp' || category === 'MobileApp'){
-      const responseContent = `You have the following information: ${data[category]}. Can you answer the following question? Question: '${question}'`;
+      const responseContent = `You have the following information: ${data[category]}. Can you answer the following question? Also, please format your answer in a readable way, adding necessary line breaks ('backwardslash n') where needed. Those line breaks but be added in your response string, so when the response pulled to front end the linebreaks are added to the text. Question: '${question}'`;
       const response = await generateChatCompletion("user", responseContent);
 
       console.log(response)
